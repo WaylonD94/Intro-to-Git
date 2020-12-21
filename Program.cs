a6ea708e2478f58da9ec9fb01181351012e033c0
Waylon Doherty <S00209027@mail.itsligo.ie>
2020-12-21 19:54:48 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_to_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Added Milk");
+        }
+    }
+}
